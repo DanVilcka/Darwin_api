f41bb51d7120bc115d7e958d53da98d4e7c60509
Daniil Vilchinskiy <danvilcka@gmail.com>
2024-08-06 19:44:25 +0300

    add .env

--- to-do_list/database.py
+++ to-do_list/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1/darwin_api"
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
